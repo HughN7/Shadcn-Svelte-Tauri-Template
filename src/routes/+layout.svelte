888c951afdcbf60fae72547bbc7fbb6c6b0b67cb
Hieu Vo <hughvo39@gmail.com>
2025-01-20 20:44:28 -0800

    Init commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import '../app.css';
+	let { children } = $props();
+</script>
+
+<div class="bg-black h-screen text-white">
+	{@render children()}
+</div>
+
---
+++ src/routes/+page.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+    let test = $state(false)
+
+
+</script>
+
+
+<div class="p-2">
+    <h1>Page</h1>
+    <button class="border-white p-2" onclick={() => test = !test}>Toggle</button>
+    {#if test}
+        <p>Test</p>
+    {/if}
+</div>
+
+<button id="decorum-tb-close">
+    Test Button
+</button>
+
+
+
+<style>
+    button#decorum-tb-close {
+        background-color: #000;
+        color: #fff;
+        border: 1px solid #fff;
+        border-radius: 5px;
+        padding: 5px 10px;
+        cursor: pointer;
+        display: fixed; 
+        z-index: 0;
+    }
+</style>
