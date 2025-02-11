c0491a71ddee66e8567dfb5b1b42c21412e896fb
Hieu Vo <hughvo39@gmail.com>
2025-02-10 22:57:13 -0800

    Remove unnecessary blank line in global store

--- src/lib/global-store.ts
+++ src/lib/global-store.ts
@@ -7,7 +7,6 @@ export const appWidth = persisted<number>('app-width', 0)
 export const leftSideBarWidth = persisted<number>('left-sidebar-width', 256)
 export const leftSideBarWidthBeforeCollapse = persisted<number>('left-sidebar-width-before-collapse', 220)
 export const leftSideBarSheet = persisted<boolean>('left-sidebar-sheet', false)
-
 export const leftSideBarHandleDrag = writable<boolean>(false)
 
 
